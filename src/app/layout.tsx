19801013d001f0798d18e29c9efdbf2e56476a7c
wenza007 <overkung4399@gmail.com>
2025-09-18 02:15:14 +0700

    fix script layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import Navbar from './components/Navbar'
-
+import Script from 'next/script'
 export const metadata = { title: 'TCAS69 Portfolio' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th">
       <head>
-        <script src="https://cdn.tailwindcss.com"></script>
+       <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
       </head>
       <body className="bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50 min-h-screen font-sans relative">
         <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] opacity-10 pointer-events-none"></div>
